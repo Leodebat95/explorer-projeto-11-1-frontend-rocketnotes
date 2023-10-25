990701835d640cf59b5279aefd94a0c1b7eb2421
Leonardo Debattisti <leonardobdebattisti@gmail.com>
2023-10-25 13:49:26 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 export const api = axios.create({
 
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://rocketnotes-api-tkj7.onrender.com'
     /* Property que contém o endereço do Back-end;
        fica mais prático já declarar ele aqui (daí, ele já fica embutido), e depois só precisa declarar as págs. específicas (ex: "/users") */
 })
